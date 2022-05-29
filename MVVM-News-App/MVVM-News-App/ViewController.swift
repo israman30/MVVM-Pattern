d522872599b24062ec6f026d99229d42b919ed07
israman <israelmanzo814@gmail.com>
2022-05-28 23:11:35 -0400

    added new project for MVVM - news app using a news api

---
+++ MVVM-News-App/MVVM-News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-News-App
+//
+//  Created by Israel Manzo on 5/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
